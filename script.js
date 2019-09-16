ef38a9737a75ab93b275b60cfbf9742e8c0a7d11
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-09-16 14:59:13 -0400

    test archive preview functionality

--- script.js
+++ script.js
@@ -247,7 +247,7 @@ function detectPupilMovement(event) {
 /* ---------------------------- EVENT HANDLERS ---------------------------- */
 $(document).ready(function() {
   setTimeout(function() {
-    ideTypingLoop();
+    //ideTypingLoop();
   }, 500);
 
   setTimeout(function() {
@@ -395,10 +395,28 @@ $(document).ready(function() {
 
   $archiveLink.on("mouseenter", function() {
     $(this).addClass("archive-link-hovered");
+
+    $(this).children(".archive-image").css("display", "none");
+    $(this).children(".archive-vid").css("display", "block");
+
+
+    document.getElementById("archive-vid-1").play();
+    document.getElementById("archive-vid-2").play();
+    document.getElementById("archive-vid-1").loop = true;
+    document.getElementById("archive-vid-2").loop = true;
   });
 
   $archiveLink.on("mouseleave", function() {
     $(this).removeClass("archive-link-hovered");
+
+    $(this).children(".archive-vid").css("display", "none");
+    $(this).children(".archive-image").css("display", "block");
+
+
+    document.getElementById("archive-vid-1").pause();
+    document.getElementById("archive-vid-2").pause();
+    document.getElementById("archive-vid-1").currentTime = 0;
+    document.getElementById("archive-vid-2").currentTime = 0;
   });
 
   $archiveLink.on("touchstart", function() {
