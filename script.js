d4f1ed9ec96f9223eae2a394096b176639a8dffe
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-09-24 14:08:51 -0400

    edit archive section(edit archive image/vid dimensions)

--- script.js
+++ script.js
@@ -433,7 +433,7 @@ function pauseArchivePreview(selectedArchiveLink, selectedArchiveVideo) {
 /* ---------------------------- EVENT HANDLERS ---------------------------- */
 $(document).ready(function() {
   setTimeout(function() {
-    ideTypingLoop();
+    //ideTypingLoop();
   }, 500);
 
   setTimeout(function() {
